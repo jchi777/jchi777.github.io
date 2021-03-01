a171918172a75eb2a0c5db74279287cbb0d78819
John Chi <jchi@ups.com>
2021-03-01 11:35:18 -0500

    update js

---
+++ MobileNative_vTEST.js
@@ -0,0 +1,25 @@
+var mobileNative = (function () {
+
+	'use strict';
+
+	var mobile = {};
+
+	mobile.urls = {
+		MOBILENATIVEINTERCEPT: "https://stTest.com/STTEST?interceptType=",
+	}
+
+
+	mobile.invokeTracktoMobile = function simplifiedTrackingToMobile(trackingNumber, trackStatusCode, trackStatusDescription){  
+		var url = mobile._getTrackUrl();
+		window.location = url;
+	} 
+
+	
+	mobile._getTrackUrl = function getTrackUrl(){
+		return mobile.urls["MOBILENATIVEINTERCEPT"] + "SimplifiedTrackingToMobile";
+	};
+
+
+return mobile;
+
+})();
